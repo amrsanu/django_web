14bf344d4702a3a4ac6b5e0107c0f67cab7d83c0
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-03-26 07:08:48 +0530

    Added Dynamic url and View

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route="january", view=views.january),
-    path(route="february", view=views.february),
+    # path(route="january", view=views.january),
+    # path(route="february", view=views.february),
+    path(route="<month>", view=views.monthly_challenges)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    """View Example to"""
+# def january(request):
+#     """View Example to"""
 
-    return HttpResponse("This Works [JANUARY]...")
+#     return HttpResponse("This Works [JANUARY]...")
 
 
-def february(request):
-    """View Example to"""
+# def february(request):
+#     """View Example to"""
 
-    return HttpResponse("This Works [FEBRUARY]...")
+#     return HttpResponse("This Works [FEBRUARY]...")
+
+
+def monthly_challenges(request, month):
+    """Dynamic View Example"""
+
+    if month == "january":
+        challenge_text = "Eat no meat for entire month"
+    elif month == "february":
+        challenge_text = "Go for walk"
+    elif month == "march":
+        challenge_text = "Hit the GYM"
+    elif month == "april":
+        challenge_text = "Drink more water"
+    elif month == "may":
+        challenge_text = "Reads books for 1Hr daily"
+    else:
+        return HttpResponseNotFound("This month is not found")
+
+    return HttpResponse(challenge_text)
